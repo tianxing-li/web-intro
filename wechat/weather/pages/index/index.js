2df8598f853cb7caef5ec32ebf12efad999805a1
tianxing.li <tianxing.li.cn@gmail.com>
2018-12-04 11:17:54 +0100

    get json data from api

--- wechat/weather/pages/index/index.js
+++ wechat/weather/pages/index/index.js
@@ -1,1 +1,16 @@
-Page({})
+Page({
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '北京市'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: res => {
+        console.log(res)
+      }
+    })
+  }
+})
