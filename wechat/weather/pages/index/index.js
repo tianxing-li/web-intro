ce3e5eed8573820c25a38941324a3cbd2286e572
tianxing.li <tianxing.li.cn@gmail.com>
2018-12-04 15:52:42 +0100

    add actual temp and weather

--- wechat/weather/pages/index/index.js
+++ wechat/weather/pages/index/index.js
@@ -1,15 +1,35 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
 Page({
+  data: {
+    nowTemp: 14 + '°',
+    nowWeather: 's'
+  },
   onLoad(){
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
-        city: '北京市'
+        city: '上海市'
       },
       header: {
         'content-type': 'application/json' // 默认值
       },
       success: res => {
-        console.log(res)
+        //console.log(res)
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather]
+        })
       }
     })
   }
