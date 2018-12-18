db8c976678471eb5e50909d7e7387c316370fe4b
tianxing.li <tianxing.li.cn@gmail.com>
2018-12-18 17:10:08 +0100

    add 2nd page about week weather

--- wechat/weather/pages/list/list.js
+++ wechat/weather/pages/list/list.js
@@ -1,3 +1,50 @@
 // pages/list/list.js
+const dayMap = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
 Page({
+  data: {
+    weekWeather: [1, 2, 3, 4, 5, 6, 7]
+  },
+  onLoad() {
+    this.getWeekWeather(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  onPullDownRefresh() {
+    this.getWeekWeather()
+  },
+  getWeekWeather(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/future',
+      data: {
+        time: new Date().getTime(),
+        city: '昆明市'
+      },
+      success : res => {
+        let result = res.data.result
+        //console.log(result)
+        this.setWeekWeather(result)
+      },
+      complete: ()=>{
+        callback && callback()
+      }
+    })
+  },
+  setWeekWeather(result) {
+    let weekWeather = []
+    for (let i = 0; i < 7; i++) {
+      let date = new Date()
+      date.setDate(date.getDate() + i)
+      weekWeather.push({
+        day: dayMap[date.getDay()],
+        date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+        temp: `${result[i].minTemp}° - ${result[i].maxTemp}°`,
+        iconPath: '/img/' + result[i].weather + '-icon.png'
+      })
+    }
+    weekWeather[0].day = 'Today'
+    this.setData({
+      weekWeather: weekWeather
+    })
+  }
 })
